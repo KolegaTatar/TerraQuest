b52452dde584c96a607e5c3327df1ae1a92c6684
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-05-03 16:11:20 +0200

    u5

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
-    emptyOutDir: true
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
   }
 })
